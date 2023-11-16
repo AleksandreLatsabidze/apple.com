c0654e2e25272697eeb09f0cc89be814ebf47458
aleksandrelcb <121313546+AleksandreLatsabidze@users.noreply.github.com>
2023-11-17 02:02:27 +0400

    add header

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/media.css">
+    <title>Apple</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-menu">
+                <ul>
+                    <li>
+                        <a href="">
+                            <img  src="image/logo.svg" alt="Apple logo" >
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">Mac</a>
+                        <div class="submenu">
+                            <div class="container">
+                                <div class="submenu-flex">
+                                    <div class="submenuflex-1">
+                                        <ul>
+                                            <li>
+                                                <span>Explore Mac</span>
+                                            </li>
+                                            <li>
+                                                <a href="">Explore All Mac</a>
+                                            </li>
+                                            <li>
+                                                <a href="">MacBook Air</a>
+                                            </li>
+                                            <li>
+                                                <a href="">MacBook Pro</a>
+                                            </li>
+                                            <li>
+                                                <a href="">iMac</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Mac mini</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Mac Studio</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Mac Pro</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Displays</a>
+                                            </li>
+                                            <li class="minitxt">
+                                                <a href="">Compare Mac</a>
+                                            </li>
+                                            <li class="minitxt">
+                                                <a href="">Why Mac</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="submenuflex-2">
+                                        <ul>
+                                            <li>
+                                                <span>
+                                                    More from Mac
+                                                </span>
+
+                                            </li>
+                                            <li>
+                                                <a href="">Mac Support</a>
+                                            </li>
+                                            <li>
+                                                <a href="">iCloud+</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="">Ipad</a>
+                    </li>
+                    <li>
+                        <a>iPhone</a>
+                    </li>
+                    <li>
+                        <a>Watch</a>
+                    </li>
+                    <li>
+                        <a>AirPods</a>
+                    </li>
+                    <li>
+                        <a>TV & Home</a>
+                    </li>
+                    <li>
+                        <a>Entertaiment</a>
+                    </li>
+                    <li>
+                        <a>Support</a>
+                    </li>
+                    <li>
+                        <a>Where to Buy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="header-view">
+            <div class="container">
+               <div class="headerview-flex">
+               <div class="iphone-14">
+                    iPhone 14 Pro
+                </div>
+                <div class="tech-buy">
+                    <ul>
+                        <li>
+                            <a>Overwiew</a>
+                        </li>
+                        <li>
+                            <a href="">Switch from Android to iPhone</a>
+                        </li>
+                        <li>
+                            <a href="">Tech Specs</a>
+                        </li>
+                        <li>
+                            <a class="buy-btn" href="">Buy</a>
+                        </li>
+                    </ul>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div class="header-video">
+           <div class="container">
+
+           <div class="video-center">
+                <video autoplay muted loop>
+                <source src="video/large.mp4" type="video/mp4">
+                </video>
+            </div>
+           </div>
+        </div>
+    </header>
+</body>
+</html>
